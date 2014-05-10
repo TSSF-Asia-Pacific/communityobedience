bc4c6bdf82c733c3095b53d89d029ac0afece7fb
Tim Local <tim@SUQLD-L0003>
2014-05-10 21:19:53 +1000

    viewport comment, should assist with mobile

--- index.gen.php
+++ index.gen.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
     <title>tssf Community Obedience</title>
     <style type="text/css">
       <?php include 'css/tssf.css'; ?>
