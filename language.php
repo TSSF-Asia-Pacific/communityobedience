d60e6787ea89ef4095072c7783872c6d677c798d
Tim White <tim@whiteitsolutions.com.au>
2019-04-01 22:07:47 +1000

    Add comment

--- language.php
+++ language.php
@@ -1,5 +1,10 @@
 <?php
 
+/* Extend this abstract class and override the public properties
+ * It's probably easier to just copy the English version. This file is more
+ * useful for code completion.
+ */
+
 abstract class language
 {
     public $name = "Language Name";
