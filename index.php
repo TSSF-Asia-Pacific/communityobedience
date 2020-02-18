3ffe35bc5917cfec33739c79780b3b42427a64cd
Tim White <tim@whiteitsolutions.com.au>
2020-02-18 21:20:48 +1000

    Update tracking code

--- index.php
+++ index.php
@@ -217,27 +217,21 @@ foreach ($translations as $lang => $translation) {
 <!-- Usage tracking -->
 <!-- Matomo -->
 <script type="text/javascript">
-    var _paq = _paq || [];
-    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-    (function () {
-        var u = "https://piwik.whiteitsolutions.com.au/";
-        _paq.push(['setTrackerUrl', u + 'piwik.php']);
-        _paq.push(['setSiteId', '22']);
-        var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-        g.type = 'text/javascript';
-        g.async = true;
-        g.defer = true;
-        g.src = u + 'piwik.js';
-        s.parentNode.insertBefore(g, s);
-    })();
+  var _paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//piwik.whiteitsolutions.com.au/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '22']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
 </script>
+<noscript><p><img src="//piwik.whiteitsolutions.com.au/matomo.php?idsite=22&amp;rec=1" style="border:0;" alt="" /></p></noscript>
 <!-- End Matomo Code -->
 
-<!-- Matomo Image Tracker-->
-<img src="https://piwik.whiteitsolutions.com.au/piwik.php?idsite=22&amp;rec=1" style="border:0" alt=""/>
-<!-- End Matomo -->
 
 </body>
 </html>
