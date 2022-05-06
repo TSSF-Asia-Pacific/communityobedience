26546f6a551dfffec030ccf5fc8d616cd8d3e183
Tim White <tim@whiteitsolutions.com.au>
2022-05-06 21:46:35 +0800

    cleanup

--- index.php
+++ index.php
@@ -1,26 +1,25 @@
 <?php
 
-// autoloader
-use languages\AbstractLanguage;
 use languages\en\english;
 use languages\ko\ko;
 use languages\zh\zh;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 require 'vendor/autoload.php';
 
-
-$loader = new \Twig\Loader\FilesystemLoader('./templates');
-$twig = new \Twig\Environment($loader);
+$loader = new FilesystemLoader('./templates');
+$twig = new Environment($loader);
 
     // Load each translation into the translations array here
     $translations['en'] = new english();
     // $translations['ko'] = new ko();
     $translations['zh'] = new zh();
 
-    $dateLocaleKeys = [
-            'en' => $translations['en']->dateLocale,
-            'zh' => $translations['zh']->dateLocale,
-    ];
+    $dateLocaleKeys = [];
+    foreach($translations as $locale => $translation) {
+        $dateLocaleKeys[$locale] = $translation->dateLocale;
+    }
 
     echo $twig->render('main.html.twig', [
         'translations' => $translations,
