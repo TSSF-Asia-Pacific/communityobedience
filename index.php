5e463cb247fd69109cea731f91047d7337c7d3cd
Tim White <tim@whiteitsolutions.com.au>
2022-08-20 22:07:27 +0800

    Initial translation attemps

--- index.php
+++ index.php
@@ -10,6 +10,15 @@ require 'vendor/autoload.php';
 
 $loader = new FilesystemLoader('./templates');
 $twig = new Environment($loader);
+$translator = new \Symfony\Component\Translation\Translator('en');
+$translator->addLoader('xliff', new \Symfony\Component\Translation\Loader\XliffFileLoader());
+$translator->addResource('xliff', './translations/community-obedience-en-AU.xlf', 'en');
+$translator->addResource('xliff', './translations/community-obedience-zh.xlf', 'zh');
+$translator->addResource('xliff', './translations/community-obedience-ko.xlf', 'ko');
+$translator->setFallbackLocales(['en']);
+
+$twig->addExtension(new \Symfony\Bridge\Twig\Extension\TranslationExtension($translator));
+
 
     // Load each translation into the translations array here
     $translations['en'] = new english();
