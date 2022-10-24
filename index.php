813cc08b09d60b8ce240e164425fc7c51a84a60e
Tim White <tim@whiteitsolutions.com.au>
2022-10-24 17:02:19 +0800

    move common function out of classes

--- index.php
+++ index.php
@@ -35,4 +35,33 @@ $twig->addExtension(new \Symfony\Bridge\Twig\Extension\TranslationExtension($tra
         'translations' => $translations,
         'dateLocaleKeys' => $dateLocaleKeys,
         'lastUpdated' => filemtime('cache.appcache'),
+        'dailyPrayersMembers' => getDailyPrayersMembersAll(),
     ]);
+
+
+function getDailyPrayersMembersAll(){
+    $dailyTemplateVars = [];
+    for ($day = 1; $day <= 31; $day++) {
+        $dailyTemplateVars[$day] = getDailyPrayersMembers($day);
+    }
+
+    return $dailyTemplateVars;
+}
+
+
+function getDailyPrayersMembers($day) {
+    // Lookup names for deceased and living members we are praying for
+    $templateVars = [];
+    foreach(range(1,3) as $region) {
+        $filename = __DIR__ . "/common/${day}_living_members_${region}.txt";
+        if (file_exists($filename)) {
+            $templateVars["living_members_${region}"] = file_get_contents($filename);
+        }
+    }
+    $deceasedMembersFilename = __DIR__ . "/common/${day}_deceased_members.txt";
+    if (file_exists($deceasedMembersFilename)) {
+        $templateVars['deceased_members'] = file_get_contents($deceasedMembersFilename);
+    }
+
+    return $templateVars;
+}
--- languages/AbstractLanguage.php
+++ languages/AbstractLanguage.php
@@ -15,21 +15,4 @@ abstract class AbstractLanguage
     public $name = "Language Name";
 
     public $dateLocale = "en"; // See https://momentjs.com/ for possible Locales
-
-    public function getDailyPrayersMembers($day) {
-        // Lookup names for deceased and living members we are praying for
-        $templateVars = [];
-        foreach(range(1,3) as $region) {
-            $filename = __DIR__ . "/../common/${day}_living_members_${region}.txt";
-            if (file_exists($filename)) {
-                $templateVars["living_members_${region}"] = file_get_contents($filename);
-            }
-        }
-        $deceasedMembersFilename = __DIR__ . "/../common/${day}_deceased_members.txt";
-        if (file_exists($deceasedMembersFilename)) {
-            $templateVars['deceased_members'] = file_get_contents($deceasedMembersFilename);
-        }
-
-        return $templateVars;
-    }
 }
