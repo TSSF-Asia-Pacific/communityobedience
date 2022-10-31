0fa421dc7c71c66c49e3b4b30182289d0a551f96
Tim White <tim@whiteitsolutions.com.au>
2022-10-31 20:26:04 +0800

    Only download if not changed

--- translations/download.sh
+++ translations/download.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+cd "$(dirname "$0")"
 
 # Load local environment file to load the localise.biz api key (LOCALISE_KEY)
 . env.local
-curl -o 'community-obedience-en-AU.xlf' "https://localise.biz/api/export/locale/en-AU.xlf?index=text&fallback=en-AU&key=$LOCALISE_KEY&format=xlf2"
-curl -o 'community-obedience-zh.xlf' "https://localise.biz/api/export/locale/zh.xlf?index=text&key=$LOCALISE_KEY&format=xlf2&fallback=en-AU"
-curl -o 'community-obedience-ko.xlf' "https://localise.biz/api/export/locale/ko.xlf?index=text&key=$LOCALISE_KEY&format=xlf2&fallback=en-AU"
+curl -o 'community-obedience-en-AU.xlf' -z 'community-obedience-en-AU.xlf' "https://localise.biz/api/export/locale/en-AU.xlf?index=text&fallback=en-AU&key=$LOCALISE_KEY&format=xlf2"
+curl -o 'community-obedience-zh.xlf' -z 'community-obedience-zh.xlf' "https://localise.biz/api/export/locale/zh.xlf?index=text&key=$LOCALISE_KEY&format=xlf2&fallback=en-AU"
+curl -o 'community-obedience-ko.xlf' -z 'community-obedience-ko.xlf' "https://localise.biz/api/export/locale/ko.xlf?index=text&key=$LOCALISE_KEY&format=xlf2&fallback=en-AU"
