5d58237d161ea91bc58386004e17b46336f8c901
Tim White <tim@whiteitsolutions.com.au>
2023-06-01 08:17:35 +1000

    Add TA transaltion

--- translations/download.sh
+++ translations/download.sh
@@ -7,7 +7,9 @@ cd "$(dirname "$0")"
 EN_MTIME=$(grep "Exported at:" community-obedience-en-AU.xlf | cut -d ':' -f 2-)
 ZH_MTIME=$(grep "Exported at:" community-obedience-zh.xlf | cut -d ':' -f 2-)
 KO_MTIME=$(grep "Exported at:" community-obedience-ko.xlf | cut -d ':' -f 2-)
+TA_MTIME=$(grep "Exported at:" community-obedience-TA.xlf | cut -d ':' -f 2-)
 
 curl -o 'community-obedience-en-AU.xlf' -z "$EN_MTIME" "https://localise.biz/api/export/locale/en-AU.xlf?index=text&fallback=en-AU&key=$LOCALISE_KEY&format=xlf2"
 curl -o 'community-obedience-zh.xlf' -z "$ZH_MTIME" "https://localise.biz/api/export/locale/zh.xlf?index=text&key=$LOCALISE_KEY&format=xlf2&fallback=en-AU"
 curl -o 'community-obedience-ko.xlf' -z "$KO_MTIME" "https://localise.biz/api/export/locale/ko.xlf?index=text&key=$LOCALISE_KEY&format=xlf2&fallback=en-AU"
+curl -o 'community-obedience-ta.xlf' -z "$TA_MTIME" "https://localise.biz/api/export/locale/ta.xlf?index=text&key=$LOCALISE_KEY&format=xlf2&fallback=en-AU"
